995e86d066204bbd993b7f1e9cd85c54bf6d7b98
apoorv-x12 <fifa.ajay219@gmail.com>
2025-07-29 20:38:38 +0530

    gouroutine call added

--- goCron/cron.go
+++ goCron/cron.go
@@ -6,11 +6,15 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
+func publish() {
+fmt.Println("Running every 1 minute", time.Now())
+}
+
 func main() {
 	c := cron.New(cron.WithSeconds())
 
-	c.AddFunc("@every 1m", func() {
-		fmt.Println("Running every 1 minute", time.Now())
+	c.AddFunc("@every 1m",func(){
+		go publish()
 	})
 
 	c.Start()
